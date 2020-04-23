10261c8d7da0ba4df8b11044248c0a09601de5fc
hibiki <hibiki@hibiki.net>
2020-04-24 08:28:16 +0900

    fettuccine

--- cogs/omikuji.py
+++ cogs/omikuji.py
@@ -1,6 +1,7 @@
 from discord.ext import commands # Bot Commands Frameworkのインポート
 import random
 
+
 # コグとして用いるクラスを定義。
 class RandomCog(commands.Cog):
 
@@ -14,7 +15,8 @@ class RandomCog(commands.Cog):
     async def omikuji(self, ctx):
         '''おみくじを引く'''
         unsei = ['大吉', '吉', '中吉', '小吉', '末吉', '凶', '大凶']
-        await ctx.send(f'{ctx.author.name}さんの今日の運勢は{unsei[random.randrange(len(unsei))]}です！')
+        num = random.randrange(len(unsei))
+        await ctx.send(f'{ctx.author.name}さんの今日の運勢は{unsei[num]}です！')
     
     @commands.command()
     async def dice(self, ctx, num=6):
@@ -25,6 +27,14 @@ class RandomCog(commands.Cog):
         except:
             num = 6
         await ctx.send(f'dice{num} = {random.randrange(num) + 1}')
+
+    @commands.command()
+    async def fettuccine(self, ctx):
+        '''おみくじを引く'''
+        flavor = ['イタリアングレープ', 'イタリアンピーチ', 'イタリアンレモン', 'カシスオレンジ', 'グレープソーダ', 'コーラ', 'ソーダ']
+        num = random.randrange(len(flavor))
+        await ctx.send(f'{ctx.author.name}さん、フェットチーネグミ{flavor[num]}味がおすすめですよ！')
+    
 
 # Bot本体側からコグを読み込む際に呼び出される関数。
 def setup(bot):

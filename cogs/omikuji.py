766c127950306a4ec9f50cf7e33b77253b8d456f
HimHim1113 <beyondlilith@gmail.com>
2020-05-04 21:23:34 +0900

    URの入れ替え

--- cogs/omikuji.py
+++ cogs/omikuji.py
@@ -18,7 +18,11 @@ class RandomCog(commands.Cog):
         num = random.randrange(len(unsei))
         #await ctx.send(f'{ctx.author.name}さんの今日の運勢は{unsei[num]}です！')
         num = random.randrange(len(unsei) - 2)
-        await ctx.send(f'{ctx.author.name}さんの今日の運勢は{unsei[6] if ctx.author.name == "雪野冬磨" else unsei[num]}です！')
+        flag = False
+        string = ctx.author.name[0:2]
+        if string == '雪野' or string == '雪の' or string == 'ゆき' or string == 'ゆき':
+            flag = True
+        await ctx.send(f'{ctx.author.name}さんの今日の運勢は{unsei[6] if flag else unsei[num]}です！')
     
     @commands.command()
     async def dice(self, ctx, num=6):

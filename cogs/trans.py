3b71cd1fda775780cd1228abfcdc3f56243c3856
HimHim1113 <beyondlilith@gmail.com>
2020-05-28 18:49:39 +0900

    transの追加

--- cogs/trans.py
+++ cogs/trans.py
@@ -10,9 +10,10 @@ class TransCog(commands.Cog):
 
     # コマンドの作成。コマンドはcommandデコレータで必ず修飾する。
     @commands.command()
-    async def trans(self, ctx, word, src, dst): # 関数名=コマンド名
-        '''翻訳[文、翻訳元言語、翻訳後言語]'''
-        await ctx.send(f'{Translator().translate(word, src=src, dest=dst).text}')
+    async def trans(self, ctx, dst, *word): # 関数名=コマンド名
+        '''翻訳[翻訳後言語、文]'''
+        word = ' '.join(word)
+        await ctx.send(f'{Translator().translate(word, dest=dst).text}')
 
 # Bot本体側からコグを読み込む際に呼び出される関数。
 def setup(bot):
